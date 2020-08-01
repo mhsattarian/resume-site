147f78cf474741a506ac30e309fe102d1d5ed515
Mohammad H. Sattarian <mh_sattarian@outlook.com>
2020-08-01 18:00:47 +0430

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,31 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { NotionRenderer } from "react-notion";
+import styles from "../styles/Home.module.css";
+import Twemoji from "react-twemoji";
 
-export default function Home() {
+const NOTION_BLOG_ID = "Mohammad-H-Sattarian-eb87a8ab1acc49f69e8ac1bb597ad0b2";
+
+export async function getStaticProps() {
+  const data = await fetch(
+    `https://notion-api.splitbee.io/v1/page/${NOTION_BLOG_ID}`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      blockMap: data,
+    },
+  };
+}
+
+export default function Home({ blockMap }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    <Twemoji options={{ className: styles.twemoji }}>
+      <div style={{ maxWidth: 768, margin: "0 auto", padding: "0 8px" }}>
+        <Head>
+          <title>Mohammad Hassan Sattarian</title>
+        </Head>
+        <NotionRenderer blockMap={blockMap} />
+      </div>
+    </Twemoji>
+  );
 }
